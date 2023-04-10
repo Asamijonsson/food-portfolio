1ff70e0eed35eb347136692e9f9b406668f2c24f
Asami <90178926+Asamijonsson@users.noreply.github.com>
2023-04-10 12:05:32 +0200

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCcEgxyLNP-7sTn7nDTllhHzA4XAfwY9kM",
+    authDomain: "rainbow-pancake.firebaseapp.com",
+    projectId: "rainbow-pancake",
+    storageBucket: "rainbow-pancake.appspot.com",
+    messagingSenderId: "482786206723",
+    appId: "1:482786206723:web:6d61d67754f45990915a36",
+    measurementId: "G-9HPL0M8W3C"
+  };
